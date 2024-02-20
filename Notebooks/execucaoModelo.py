3238ad0be213b343d5e1cbfb3f92f104ccc8709d
Matheus <46912481+Mat780@users.noreply.github.com>
2024-02-20 11:12:41 -0400

    docs: Adiciona comentarios e estiliza

--- Notebooks/execucaoModelo.py
+++ Notebooks/execucaoModelo.py
@@ -28,38 +28,51 @@ def pegarMateria():
 
         if (materia >= len(materias) and materia < 0):
             materia = None
-            print("Nenhuma matéria encontrada com esse numero")
+            print("\033[41mNenhuma matéria encontrada com esse numero\033[0m")
     
     return materia
 
 while quebrarWhile == False:
+
+    # Menu de interacao com o usuario
     print("* Menu *".center(espacamento, '-'))
     print("1 - Fazer pergunta ".ljust(espacamento, '-'))
     print("0 - Sair ".ljust(espacamento, '-'), flush=True)
     opcao = int(input())
 
+    # Opcao de saida quebra o while
     if opcao == 0: quebrarWhile = True
+
+    # Opcao para fazer uma pergunta e predize-la
     elif opcao == 1:
         print(" Digite sua pergunta ".center(espacamento, '-'), flush=True)
         texto = input()
-
+        print(f"Pergunta: {texto}")
         print("".center(espacamento, '-'))
         print(" Agora nos diga o numero da matéria ".center(espacamento, '-'), flush=True)
-        indice_materia = pegarMateria()
 
+        # Chama uma funcao para imprimir as materias possiveis dinamicamente
+        indice_materia = pegarMateria()
+        
+        # Registra o tempo de inicio para o calculo do tempo gasto pela classificacao
         tempo_inicio = time.perf_counter()
+
+        # Classifica o texto e nos retorna a label prevista e seu score
         resultado = classificador(texto)[0]
+
+        # Registra o tempo ao fim da classificacao para calculo do tempo de resposta
         tempo_fim = time.perf_counter()
 
         print("".center(espacamento, "-"))
         print(f"Matéria original: {materias[indice_materia]}")
         print(f"Matéria prevista: {resultado['label']}")
-        print(f"Tempo de resposta: {tempo_fim - tempo_inicio} segundos")
+        print(f"\033[33mPontuação da previsão: {resultado['score']}\033[0m")
+        print(f"\033[36mTempo de resposta: {tempo_fim - tempo_inicio} segundos\033[0m")
 
         if resultado['label'] == materias[indice_materia]:
-            print("A IA acertou a matéria!")
+            print("\033[32mA IA acertou a matéria!\033[0m")
         else:
-            print("A IA errou a matéria, talvez precise de um melhor treinamento")
+            print("\033[31mA IA errou a matéria, talvez precise de um melhor treinamento\033[0m")
         
 
     else: print(f"Opção {opcao} não existe no Menu")
